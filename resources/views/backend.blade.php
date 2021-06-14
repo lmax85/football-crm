4c780a1000b9b87f6d2bf19516b10eadb824d2f8
Maxim Lanin <laninmaksim@gmail.com>
2021-06-14 20:27:47 +0700

    added vue-admin to project

---
+++ resources/views/backend.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script type='text/javascript'>
+        window.Laravel = <?php echo json_encode([
+            'csrfToken' => csrf_token(),
+        ]); ?>
+    </script>
+</head>
+<body>
+<h1>Backend admin panel here</h1>
+<div id="app"></div>
+<script src="{{ mix('/js/backend.js') }}"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,38 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+mix.webpackConfig({
+    resolve: {
+        alias: {
+        '@': path.resolve(__dirname, 'resources/js/backend'),
+        '^': path.resolve(__dirname, 'resources/js/frontend'),
+        'share': path.resolve(__dirname, 'resources/share'),
+        'lang': path.resolve(__dirname, 'resources/share/lang'),
+        }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.svg$/,
+                loader: 'svg-sprite-loader',
+                include: [path.resolve(__dirname, 'resources/backend/icons/svg')],
+                options: {
+                symbolId: 'icon-[name]'
+                }
+            }
+        ],
+    }
+}).babelConfig({
+    plugins: ['dynamic-import-node']
+});
+
+mix.js('resources/js/frontend/main.js', 'public/js/frontend.js')
+  .js('resources/js/backend/main.js', 'public/js/backend.js')
+  .sass('resources/sass/frontend/app.scss', 'public/css/frontend.css')
+  .sass('resources/sass/backend/app.scss', 'public/css/backend.css')
+  .options({
+    postCss: [
+      require('autoprefixer'),
+    ],
+  });
+  // .version();
